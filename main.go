5ab2cbd74280177e78280200f519334afc8ca5af
Whitney Lampkin <18070772+WhitneyLampkin@users.noreply.github.com>
2023-09-13 12:49:03 -0500

    copy and save readme teamplate to working dir

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"strconv"
+	"time"
 )
 
 // A userInput stores the options provided by the user.
@@ -12,6 +15,7 @@ type userInput struct {
 	template string
 	name     string
 	imageUrl string
+	// TODO: add optional user input variable for destination filepath
 }
 
 func main() {
@@ -26,6 +30,9 @@ func main() {
 	if err != nil {
 		exitGracefully(err)
 	}
+
+	// Generate new file
+	generateFile(inputs)
 
 	fmt.Println(inputs)
 }
@@ -66,7 +73,73 @@ func getUserInput() (userInput, error) {
 	return userInput{*template, *name, *imageUrl}, nil
 }
 
+// Returns the filepath of the newly create file
+func generateFile(inputs userInput) (string, error) {
+	templatePath := "./templates/readme-template.md"
+
+	// TODO: Determine the template file to use with a switch statement when other templates are added
+	// TODO: Conditionally reset the templatePath in the future
+	if !(inputs.template == "readme-template" || inputs.template == "readme") {
+		return "", errors.New("the template filepath is no longer valid. please try again")
+	}
+
+	// Check that template filepath still exists
+	if _, err := validateTemplatePath(templatePath); err != nil {
+		exitGracefully(err)
+	}
+
+	// Open template file
+	newDocument, err := os.Open(templatePath)
+	// Check for errors
+	check(err)
+	// Ensure the file is closed after usage
+	defer newDocument.Close()
+
+	// TODO: Create and call a helper function to replace templated values
+
+	// Get working directory
+	workingDir, err := os.Getwd()
+	if err != nil {
+		return "", fmt.Errorf("error setting %s as the working directory", workingDir)
+	}
+
+	// Create new destination file location
+	currentTime := time.Now().UTC().Nanosecond()
+	timeString := strconv.Itoa(currentTime)
+	destination, err := os.Create(workingDir + "/" + "README_" + timeString + ".md")
+	if err != nil {
+		return "", errors.New("error creating the new file destination")
+	}
+	defer destination.Close()
+
+	// Using io.Copy() since no replacements are being made at this time
+	newFile, err := io.Copy(destination, newDocument)
+	if newFile == 0 || err != nil {
+		return "", errors.New("cannot create new file at this time, please try again")
+	}
+
+	fmt.Println(destination.Name())
+
+	return "", nil
+}
+
+func validateTemplatePath(templatePath string) (bool, error) {
+	// Validate file exists
+	if _, err := os.Stat(templatePath); err != nil && os.IsNotExist(err) {
+		return false, fmt.Errorf("file %s does not exist", templatePath)
+	}
+
+	// If no errors, the file is valid.
+	return true, nil
+}
+
 func exitGracefully(err error) {
 	fmt.Fprintf(os.Stderr, "error: %v\n", err)
 	os.Exit(1)
+}
+
+func check(e error) {
+	if e != nil {
+		exitGracefully(e)
+	}
 }
